97e4985a11d84dd39349dba3344eaca72e1afdc7
SLIVTIS <leobardo.salinasvl@gmail.com>
2025-05-14 12:58:09 -0600

    chore: actualizar versión de Flutter y NDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.cine_vault"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.1.13356709"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
